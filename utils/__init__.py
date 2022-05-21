a772008fea20da9a84e98ff4f1febc7ae148a683
Ivan Pupkin <ivan.pupkin@ank.app>
2022-05-21 20:08:51 -0300

    Adjust hexadecimal encoding

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,38 +1,39 @@
 import os
 import PirFile
+from binascii import hexlify
 
 
 class PirUtils:
     @staticmethod
     def read_pir_file(file_path: str | os.PathLike[str]) -> PirFile:
         with open(file_path, 'rb') as file:
-            file_signature = [bytes(file.read(2)) for i in range(4)]
-            unsigned_int_version = int(file.read(4))
-            info_size = int(file.read(4))
-            reserved_1 = int(file.read(4))
-            reserved_2 = int(file.read(4))
-            sample_rate_float = float(file.read(4))
-            sample_rate_int = int(file.read(4))
-            pir_length = int(file.read(4))
-            input_device = int(file.read(4))
-            device_sensitivity = float(file.read(4))
-            measurement_type = int(file.read(4))
-            averaging_type = int(file.read(4))
-            number_of_averages = int(file.read(4))
-            bfiltered = int(file.read(4))
-            generator_type = int(file.read(4))
-            peak_left = float(file.read(4))
-            peak_right = float(file.read(4))
-            generator_subtype = int(file.read(4))
+            file_signature = [bytes(file.read(1)) for i in range(4)]
+            unsigned_int_version = int(hexlify(file.read(4)), 16)
+            info_size = int(hexlify(file.read(4)), 16)
+            reserved_1 = int(hexlify(file.read(4)), 16)
+            reserved_2 = int(hexlify(file.read(4)), 16)
+            sample_rate_float = float(hexlify(file.read(4)))
+            sample_rate_int = int(hexlify(file.read(4)), 16)
+            pir_length = int(hexlify(file.read(4)), 16)
+            input_device = int(hexlify(file.read(4)), 16)
+            device_sensitivity = float(hexlify(file.read(4)))
+            measurement_type = int(hexlify(file.read(4)), 16)
+            averaging_type = int(hexlify(file.read(4)), 16)
+            number_of_averages = int(hexlify(file.read(4)), 16)
+            bfiltered = int(hexlify(file.read(4)), 16)
+            generator_type = int(hexlify(file.read(4)), 16)
+            peak_left = float(hexlify(file.read(4)))
+            peak_right = float(hexlify(file.read(4)))
+            generator_subtype = int(hexlify(file.read(4)), 16)
 
             if unsigned_int_version >= 5:
-                cursor_position = int(file.read(4))
-                marker_position = int(file.read(4))
+                cursor_position = int(hexlify(file.read(4)), 16)
+                marker_position = int(hexlify(file.read(4)), 16)
             else:
-                reserved_3 = float(file.read(4))
-                reserved_4 = float(file.read(4))
+                reserved_3 = float(hexlify(file.read(4)))
+                reserved_4 = float(hexlify(file.read(4)))
 
-            pir_data = [float(file.read(4)) for i in range(pir_length)]
+            pir_data = [float(hexlify(file.read(4))) for i in range(pir_length)]
             info_text = ''.join(file.read())
 
         return PirFile(
