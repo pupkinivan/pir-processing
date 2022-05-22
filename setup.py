2b53c76c561f81686fe30588f7c15f1cfcab0149
Ivan Pupkin <ivan.pupkin@ank.app>
2022-05-22 19:04:12 -0300

    Adjust requirements and setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ setup(
     name='pir-processing',
     version='0.0.1',
     packages=['utils'],
+    py_modules=["pir.py"],
     url='https://github.com/pupkinivan/pir-processing',
     license='',
     author='Ivan Pupkin',
     author_email='ivopupkin20@gmail.com',
-    description='Processing utilities for ARTA\'s PIR file specification.'
+    description='Processing utilities for ARTA\'s PIR file specification.',
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ]
 )
