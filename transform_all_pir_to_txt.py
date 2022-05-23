71e8e42d4cab30739ddf4e2e47fc6ae6c7112d1c
Ivan Pupkin <ivan.pupkin@ank.app>
2022-05-22 21:46:29 -0300

    Add CSV/TXT option for exporting PIR data.

--- transform_all_pir_to_txt.py
+++ transform_all_pir_to_txt.py
@@ -10,6 +10,8 @@ import argparse
 from concurrent.futures import ThreadPoolExecutor
 from pir import PirFile
 
+SAVE_AS_CSV = False
+
 
 def scan_for_pir_files_in_directory(directory: str | os.PathLike[str]):
     return set(filter(lambda filename: (filename[-3:].lower() == "pir"), os.listdir(directory)))
@@ -30,8 +32,11 @@ def transform_all_pir_files(directory: [str, os.PathLike] = "."):
 
 def read_and_save_pir_as_txt(path_to_file: str | os.PathLike[str]) -> PirFile:
     pir_file_data = PirFile.of(path_to_file)
-    txt_file_name = str(path_to_file).split('.')[-2] + ".txt"
-    save_pir_data_as_txt(txt_file_name, pir_file_data)
+    output_file_name = str(path_to_file).split('.')[-2] + (".csv" if SAVE_AS_CSV else ".txt")
+    if SAVE_AS_CSV:
+        save_pir_data_as_csv(output_file_name, pir_file_data)
+    else:
+        save_pir_data_as_txt(output_file_name, pir_file_data)
     return pir_file_data
 
 
@@ -42,12 +47,23 @@ def save_pir_data_as_txt(path_to_file: str | os.PathLike[str], pir_file: PirFile
             txt_writer.writerow([pir_row])
 
 
+def save_pir_data_as_csv(path_to_file: str | os.PathLike[str], pir_file: PirFile):
+    with open(path_to_file, 'w') as output_file:
+        txt_writer = csv.writer(output_file, delimiter=',')
+        txt_writer.writerow(["Time [s]", "Amplitude [V]"])
+        for pir_row in pir_file.get_ir():
+            txt_writer.writerow(pir_row.tolist())
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Enter a directory from which to source PIR files and transform them to .txt"
     )
     parser.add_argument("directory_path", help="Path to the PIR directory")
+    parser.add_argument("--save_as_csv", "--csv", required=False, action='store_const', const=True, default=False,
+                        help="Save the file as a CSV, with amplitude along with a time column")
     args = parser.parse_args()
+    SAVE_AS_CSV = args.save_as_csv
 
     if args.directory_path is None:
         logging.error("Expected one directory")
