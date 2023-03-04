47b3a90f555e01b28773ea71e47e0f521239ead7
ips <ipupkin@untref.edu.ar>
2023-03-04 16:43:33 -0300

    Add dataclasses and fix logging

--- tests/mock_data/__init__.py
+++ tests/mock_data/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 from pir_processing.pir import PirFile
 
 path = Path("./tests/mock_data/untitled_ver_deg0.pir")
-PIR_FILE = PirFile.of(path)
+PIR_FILE = PirFile.from_file_path(path)
